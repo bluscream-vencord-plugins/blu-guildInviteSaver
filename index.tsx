1f07f2498622c8b14a7198008d4cb40a93f447c6
Bluscream <Bluscream@users.noreply.github.com>
2026-02-16 02:53:10 +0100

    Refactor: Adhered to Gold Standard conventions

--- index.tsx
+++ index.tsx
@@ -21,18 +21,8 @@ import { registerSharedContextMenu } from "./utils/menus";
 import { settings } from "./settings";
 // endregion Imports
 
-// region PluginInfo
-export const pluginInfo = {
-    id: "guildInviteSaver",
-    name: "GuildInviteSaver",
-    description: "Automatically collects and saves invite links before leaving a server",
-    color: "#7289da",
-    authors: [
-        { name: "Bluscream", id: 467777925790564352n },
-        { name: "Assistant", id: 0n }
-    ],
-};
-// endregion PluginInfo
+import { pluginInfo } from "./info";
+export { pluginInfo };
 
 // region Variables
 const logger = new Logger(pluginInfo.id, pluginInfo.color);
---
+++ info.ts
@@ -0,0 +1,10 @@
+export const pluginInfo = {
+    id: "guildInviteSaver",
+    name: "GuildInviteSaver",
+    description: "Automatically collects and saves invite links before leaving a server",
+    color: "#7289da",
+    authors: [
+        { name: "Bluscream", id: 467777925790564352n },
+        { name: "Assistant", id: 0n }
+    ],
+};
